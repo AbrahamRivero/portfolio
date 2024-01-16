2fe983503414940e3d5ad338d4d833ae87a37484
AbrahamRivero <riveromartinezabraham@gmail.com>
2024-01-15 19:52:39 -0500

    feat: :art: Arreglando el about

--- lib/data.ts
+++ lib/data.ts
@@ -106,6 +106,20 @@ export const projectsData = [
 		tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
 		imageUrl: wordanalyticsImg,
 	},
+	{
+		title: 'Word Analytics',
+		description:
+			'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+		tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+		imageUrl: wordanalyticsImg,
+	},
+	{
+		title: 'Word Analytics',
+		description:
+			'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+		tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+		imageUrl: wordanalyticsImg,
+	},
 ] as const
 
 export const skillsData = [
